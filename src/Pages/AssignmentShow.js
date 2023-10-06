82878be295f010501b46a0859a75643e6536a1fa
yashu119 <himanshu7007014@gmail.com>
2023-10-06 17:26:57 +0530

    firstf

--- src/Pages/AssignmentShow.js
+++ src/Pages/AssignmentShow.js
@@ -127,57 +127,57 @@ function AssignmentShow() {
               <span className="text-rose-600">Due Date: </span>
               {new Date(assignment.due).toLocaleDateString()}
             </p>
-            {Status.some(item => item._id === id) ? '' :
-
-              (
-
-                <>
-                  <p className="mt-6 text-xl font-extrabold leading-tight text-center sm:text-xl">
-                    <span className="text-green-600">Submit</span> Task
-                  </p>
-
-                  <form className="flex flex-col items-center mt-3 space-x-6" onSubmit={handleSubmit}>
-                    <label className="block">
-                      <span className="sr-only">Assignment Text</span>
-                      <input
-                        required
-                        type="text"
-                        value={textValue} // Bind the input value to the state variable
-                        onChange={(e) => setTextValue(e.target.value)} // Update the state when input changes
-                        className="block w-full p-2 border border-red-400 rounded-md text-md text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100"
-                        placeholder="Enter Your Project Link"
-                      />
-                    </label>
-
-                    <button
-                      type="submit"
-                      className="px-8 py-2 mt-4 text-lg text-white border-0 rounded bg-rose-500 focus:outline-none hover:bg-rose-600"
-                    >
-                      {Loading ? "" : "Submit"}{' '}
-                      <svg
-                        width="20"
-                        height="20"
-                        fill="currentColor"
-                        className="ml-2 animate-spin"
-                        viewBox="0 0 1792 1792"
-                        xmlns="http://www.w3.org/2000/svg"
-                        hidden={Loading ? false : true}
-                      >
-                        <path d="M526 1394q0 53-37.5 90.5t-90.5 37.5q-52 0-90-38t-38-90q0-53 37.5-90.5t90.5-37.5 90.5 37.5 37.5 90.5zm498 206q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-704-704q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm1202 498q0 52-38 90t-90 38q-53 0-90.5-37.5t-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-964-996q0 66-47 113t-113 47-113-47-47-113 47-113 113-47 113 47 47 113zm1170 498q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-640-704q0 80-56 136t-136 56-136-56-56-136 56-136 136-56 136 56 56 136zm530 206q0 93-66 158.5t-158 65.5q-93 0-158.5-65.5t-65s.5-158.5q0-92 65.5-158t158.5-66q92 0 158 66t66 158z"></path>
-                      </svg>
-                    </button>
-                  </form>
-                </>
-              )
-
-            }
+         
           </div>
           <img
-            className="my-4"
+            className="my-2"
             src={assignment.image_link}
           />
         </div>
+        {Status.some(item => item._id === id) ? '' :
 
+(
+
+  <>
+    <p className="mt-6 text-xl font-extrabold leading-tight text-center sm:text-xl">
+      <span className="text-green-600">Submit</span> Task
+    </p>
+
+    <form className="flex flex-col items-center mt-3 space-x-6" onSubmit={handleSubmit}>
+      <label className="block">
+        <span className="sr-only">Task: </span>
+        <input
+          required
+          type="text"
+          value={textValue} // Bind the input value to the state variable
+          onChange={(e) => setTextValue(e.target.value)} // Update the state when input changes
+          className="block w-full p-2 border border-red-400 rounded-md text-md text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100"
+          placeholder="Enter Your Project Link"
+        />
+      </label>
+
+      <button
+        type="submit"
+        className="px-8 py-2 mt-4 text-lg text-white border-0 rounded bg-rose-500 focus:outline-none hover:bg-rose-600"
+      >
+        {Loading ? "" : "Submit"}{' '}
+        <svg
+          width="20"
+          height="20"
+          fill="currentColor"
+          className="ml-2 animate-spin"
+          viewBox="0 0 1792 1792"
+          xmlns="http://www.w3.org/2000/svg"
+          hidden={Loading ? false : true}
+        >
+          <path d="M526 1394q0 53-37.5 90.5t-90.5 37.5q-52 0-90-38t-38-90q0-53 37.5-90.5t90.5-37.5 90.5 37.5 37.5 90.5zm498 206q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-704-704q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm1202 498q0 52-38 90t-90 38q-53 0-90.5-37.5t-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-964-996q0 66-47 113t-113 47-113-47-47-113 47-113 113-47 113 47 47 113zm1170 498q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-640-704q0 80-56 136t-136 56-136-56-56-136 56-136 136-56 136 56 56 136zm530 206q0 93-66 158.5t-158 65.5q-93 0-158.5-65.5t-65s.5-158.5q0-92 65.5-158t158.5-66q92 0 158 66t66 158z"></path>
+        </svg>
+      </button>
+    </form>
+  </>
+)
+
+}
       </div>
     </>
   );
